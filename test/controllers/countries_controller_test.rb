5ba8892edbf544e7120d1b49f9f279d333c66b04
Matteo <webmaster@meinzeug.de>
2016-01-05 18:06:57 +0100

    added controller tests

--- test/controllers/countries_controller_test.rb
+++ test/controllers/countries_controller_test.rb
@@ -3,6 +3,17 @@ require 'test_helper'
 class CountriesControllerTest < ActionController::TestCase
   fixtures :all
 
+  START_OF_TIME_FORMATTED = '0000-01-01'
+  START_OF_TIME = 0
+  END_OF_TIME_FORMATTED = '9999-12-31'
+  END_OF_TIME = 99999999
+  TODAY_FORMATTED = Date.today.strftime('%Y-%m-%d')
+  TODAY = Date.today.strftime('%Y%m%d').to_i
+  TOMORROW_FORMATTED = Date.tomorrow.strftime('%Y-%m-%d')
+  TOMORROW = Date.tomorrow.strftime('%Y%m%d').to_i
+  YESTERDAY_FORMATTED = Date.yesterday.strftime('%Y-%m-%d')
+  YESTERDAY = Date.yesterday.strftime('%Y%m%d').to_i
+
   test "should get index at present" do
     get :index
     assert_response :success
@@ -20,38 +31,46 @@ class CountriesControllerTest < ActionController::TestCase
   test "should get germany at present day" do
     get :show, {'id' => 'DEU'}
     assert_response :success
+    assert_equal 'Germany', json_response['name']
     assert_equal "DEU", json_response['identity']
     assert_equal 19901003, json_response['effective_from']
-    assert_equal 99999999, json_response['effective_to']
+    assert_equal END_OF_TIME, json_response['effective_to']
   end
 
   test "should get germany at specific date" do
     get :show, {'id' => 'DEU', 'scd_date' => '1949-01-01'}
     assert_response :success
+    assert_equal 'Germany', json_response['name']
     assert_equal "DEU", json_response['identity']
-    assert_equal 0, json_response['effective_from']
+    assert_equal START_OF_TIME, json_response['effective_from']
     assert_equal 19491007, json_response['effective_to']
   end
 
   test "should create a new static country" do
     post :create, {country: {name: 'Static Country', code: 'STC'}}
     assert_response :success
+    # return the created period
+    assert_equal 'Static Country', json_response['name']
     assert_equal "STC", json_response['identity']
-    assert_equal 0, json_response['effective_from']
-    assert_equal 99999999, json_response['effective_to']
+    assert_equal START_OF_TIME, json_response['effective_from']
+    assert_equal END_OF_TIME, json_response['effective_to']
   end
 
   test "should create a new country with start date" do
     post :create, {country: {name: 'Country with Start Date', code: 'CWS', effective_from: '1949-01-01'}}
     assert_response :success
+    # return the created period
+    assert_equal 'Country with Start Date', json_response['name']
     assert_equal "CWS", json_response['identity']
     assert_equal 19490101, json_response['effective_from']
-    assert_equal 99999999, json_response['effective_to']
+    assert_equal END_OF_TIME, json_response['effective_to']
   end
 
   test "should create a new country with start date and end date" do
     post :create, {country: {name: 'Country with Start Date and End Date', code: 'CSE', effective_from: '1949-01-01', effective_to: '1990-10-03'}}
     assert_response :success
+    # return the created period
+    assert_equal 'Country with Start Date and End Date', json_response['name']
     assert_equal "CSE", json_response['identity']
     assert_equal 19490101, json_response['effective_from']
     assert_equal 19901003, json_response['effective_to']
@@ -72,12 +91,121 @@ class CountriesControllerTest < ActionController::TestCase
   test "should get periods by identity" do
     get :periods_by_identity, {'id' => 'DEU'}
     assert_response :success
-    assert_equal '0000-01-01', json_response[0]['start']
+    assert_equal START_OF_TIME_FORMATTED, json_response[0]['start']
     assert_equal '1949-10-07', json_response[0]['end']
     assert_equal '1949-10-07', json_response[1]['start']
     assert_equal '1990-10-03', json_response[1]['end']
     assert_equal '1990-10-03', json_response[2]['start']
-    assert_equal '9999-12-31', json_response[2]['end']
+    assert_equal END_OF_TIME_FORMATTED, json_response[2]['end']
+    assert_nil json_response[3]
+  end
+
+  test "should update a static country generating a new period starting today" do
+    patch :update, id: 'CL', country: {name: 'New Caledonia', code: 'CL'}
+    assert_response :success
+    # return the created period
+    assert_equal 'New Caledonia', json_response['name']
+    assert_equal 'CL', json_response['identity']
+    assert_equal TODAY, json_response['effective_from']
+    assert_equal END_OF_TIME, json_response['effective_to']
+
+    # check all periods
+    get :periods_by_identity, {'id' => 'CL'}
+    assert_response :success
+    assert_equal START_OF_TIME_FORMATTED, json_response[0]['start']
+    assert_equal TODAY_FORMATTED, json_response[0]['end']
+    assert_equal TODAY_FORMATTED, json_response[1]['start']
+    assert_equal END_OF_TIME_FORMATTED, json_response[1]['end']
+    assert_nil json_response[2]
+  end
+
+  test "should update a static country generating a new period starting in the future" do
+    patch :update, id: 'CL', country: {name: 'Caledonia of tomorrow', code: 'CL', effective_from: TOMORROW_FORMATTED}
+    assert_response :success
+    # return the created period
+    assert_equal 'Caledonia of tomorrow', json_response['name']
+    assert_equal 'CL', json_response['identity']
+    assert_equal TOMORROW, json_response['effective_from']
+    assert_equal END_OF_TIME, json_response['effective_to']
+
+    # check all periods
+    get :periods_by_identity, {'id' => 'CL'}
+    assert_response :success
+    assert_equal START_OF_TIME_FORMATTED, json_response[0]['start']
+    assert_equal TOMORROW_FORMATTED, json_response[0]['end']
+    assert_equal TOMORROW_FORMATTED, json_response[1]['start']
+    assert_equal END_OF_TIME_FORMATTED, json_response[1]['end']
+    assert_nil json_response[2]
+  end
+
+  test "should update a static country generating a new period starting in the past" do
+    patch :update, id: 'CL', country: {name: 'Caledonia of yesterday', code: 'CL', effective_from: YESTERDAY_FORMATTED}
+    assert_response :success
+    # return the created period
+    assert_equal 'Caledonia of yesterday', json_response['name']
+    assert_equal 'CL', json_response['identity']
+    assert_equal YESTERDAY, json_response['effective_from']
+    assert_equal END_OF_TIME, json_response['effective_to']
+
+    # check all periods
+    get :periods_by_identity, {'id' => 'CL'}
+    assert_response :success
+    assert_equal START_OF_TIME_FORMATTED, json_response[0]['start']
+    assert_equal YESTERDAY_FORMATTED, json_response[0]['end']
+    assert_equal YESTERDAY_FORMATTED, json_response[1]['start']
+    assert_equal END_OF_TIME_FORMATTED, json_response[1]['end']
+    assert_nil json_response[2]
+  end
+
+  test "should unterminate a static country at today" do
+    delete :destroy, id: 'CL'
+    assert_response :success
+    # return the destroyed period in case of implementing a restore functionality
+    assert_equal 'Eternal Caledonia', json_response['name']
+    assert_equal 'CL', json_response['identity']
+    assert_equal START_OF_TIME, json_response['effective_from']
+    assert_equal END_OF_TIME, json_response['effective_to']
+
+    # check all periods
+    get :periods_by_identity, {'id' => 'CL'}
+    assert_response :success
+    assert_equal START_OF_TIME_FORMATTED, json_response[0]['start']
+    assert_equal TODAY_FORMATTED, json_response[0]['end']
+    assert_nil json_response[1]
+  end
+
+  test "should unterminate a static country in the future" do
+    delete :destroy, id: 'CL', country: {effective_from: TOMORROW_FORMATTED}
+    assert_response :success
+    # return the destroyed period in case of implementing a restore functionality
+    assert_equal 'Eternal Caledonia', json_response['name']
+    assert_equal 'CL', json_response['identity']
+    assert_equal START_OF_TIME, json_response['effective_from']
+    assert_equal END_OF_TIME, json_response['effective_to']
+
+    # check all periods
+    get :periods_by_identity, {'id' => 'CL'}
+    assert_response :success
+    assert_equal START_OF_TIME_FORMATTED, json_response[0]['start']
+    assert_equal TOMORROW_FORMATTED, json_response[0]['end']
+    assert_nil json_response[1]
+  end
+
+  test "should unterminate a static country in the past" do
+    delete :destroy, id: 'CL', country: {effective_from: YESTERDAY_FORMATTED}
+    assert_response :success
+    # return the destroyed period in case of implementing a restore functionality
+    assert_equal 'Eternal Caledonia', json_response['name']
+    assert_equal 'CL', json_response['identity']
+    assert_equal START_OF_TIME, json_response['effective_from']
+    assert_equal END_OF_TIME, json_response['effective_to']
+
+    # check all periods
+    get :periods_by_identity, {'id' => 'CL'}
+    assert_response :success
+    assert_equal START_OF_TIME_FORMATTED, json_response[0]['start']
+    assert_equal YESTERDAY_FORMATTED, json_response[0]['end']
+    assert_nil json_response[1]
   end
 
 end
--- test/dummy/app/controllers/countries_controller.rb
+++ test/dummy/app/controllers/countries_controller.rb
@@ -10,7 +10,7 @@ class CountriesController < ApplicationController
 
   def show
     begin
-      render json: Country.at_present_or!(params[:scd_date]).find_by_identity!(params[:id])
+      render json: Country.find_by_identity_at_present_or!(params[:id],params[:scd_date])
     rescue Exception => e
       render :json => {:error => e.message}, :status => :internal_server_error
     end
@@ -20,6 +20,27 @@ class CountriesController < ApplicationController
     begin
       country = Country.create_identity!(map_countries_params,map_countries_effective_from,map_countries_effective_to)
       render :json => country
+    rescue Exception => e
+      render :json => {:error => e.message}, :status => :internal_server_error
+    end
+  end
+
+  def update
+    begin
+      new_iteration = Country.create_iteration(params[:id],map_countries_params,map_countries_effective_from)
+
+      render :json => new_iteration
+    rescue Exception => e
+      render :json => {:error => e.message}, :status => :internal_server_error
+    end
+  end
+
+  def destroy
+    begin
+      old_country = Country.find_by_identity_at_present_or!(params[:id],map_countries_effective_from)
+      Country.terminate_identity(params[:id],map_countries_effective_from)
+
+      render :json => old_country
     rescue Exception => e
       render :json => {:error => e.message}, :status => :internal_server_error
     end
@@ -40,10 +61,10 @@ class CountriesController < ApplicationController
   end
 
   def map_countries_effective_from
-    params[:country][:effective_from]
+    params[:country][:effective_from] rescue nil
   end
 
   def map_countries_effective_to
-    params[:country][:effective_to]
+    params[:country][:effective_to] rescue nil
   end
 end
